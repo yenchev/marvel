e0901e7b5617204898bedbee4e966de4e25d10e7
Illia Yenchev <illiayenchev@gmail.com>
2023-07-20 07:01:09 +0200

    rou

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -12,20 +12,20 @@ const AppHeader = () => {
   return (
     <header className="app__header">
       <h1 className="app__title">
-        <Link to="/" onClick={() => setActiveTab("")}>
+        <Link to=" /" onClick={() => setActiveTab("")}>
           <span>Marvel</span> information portal
         </Link>
       </h1>
       <nav className="app__menu">
         <ul>
           <li>
-            <NavLink to="/" end className="nav-link" activeClassName={activeTab === "Characters" ? "active" : ""} onClick={() => handleNavLinkClick("Characters")}>
+            <NavLink to=" /" end className="nav-link" activeClassName={activeTab === "Characters" ? "active" : ""} onClick={() => handleNavLinkClick("Characters")}>
               Characters
             </NavLink>
           </li>
           &nbsp;/&nbsp;
           <li>
-            <NavLink to="/comics" className="nav-link" activeClassName={activeTab === "Comics" ? "active" : ""} onClick={() => handleNavLinkClick("Comics")}>
+            <NavLink to="/comics/" className="nav-link" activeClassName={activeTab === "Comics" ? "active" : ""} onClick={() => handleNavLinkClick("Comics")}>
               Comics
             </NavLink>
           </li>
